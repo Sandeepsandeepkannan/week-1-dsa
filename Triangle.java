81e82c92e43c851bb8f24ed8137a8a335b441a00
Sandeep <sandeepsandeepkannan16@gmail.com>
2026-08-26 22:27:28 +0530

    added the triangle problem

---
+++ Triangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Triangle {
+    
+    public static void main(String []args){
+   
+        // Scanner scan=new Scanner(System.in);
+int  a=45,b=45,c=45;
+
+ if((a+b+c)==180){
+     System.out.println("it is a triangle")  ; 
+    }
+    else{
+        System.out.println("it is not  triangle")  ; 
+    }
+}}
