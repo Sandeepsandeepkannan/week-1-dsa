ab0362e9ee891900ec5a3490c80fd0a37998f83f
Sandeep <sandeepsandeepkannan16@gmail.com>
2026-08-26 22:20:32 +0530

    added input taking problem

---
+++ Problem1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Problem1 {
+ public static void main(String[]args){
+
+    Scanner scan = new Scanner(System.in);
+    int num= scan.nextInt();
+    String name=scan.next();
+    char ch=scan.next().charAt(0);
+
+    System.out.println(num);
+    System.out.println(name);
+    System.out.println(ch);
+
+ }   
+}
