bd6422c2f65948a7411b4a8b8e3983830a06f768
Sandeep <sandeepsandeepkannan16@gmail.com>
2026-08-27 22:55:25 +0530

    added the two value

---
+++ Twoval.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public  class Twoval {
+
+    public static void main(String []args){
+
+        Scanner scan=new Scanner(System.in);
+
+        int a =scan.nextInt();
+        int b =scan.nextInt();
+
+        for (int i=1;i<=b;i++){
+            System.out.println(a);
+        }
+    }
+    
+} 
